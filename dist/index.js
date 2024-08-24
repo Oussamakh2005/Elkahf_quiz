299ea491f413d6ddfbc225069deb808f5c955591
Oussama <khelefoussama2005@gmail.com>
2024-08-24 21:14:42 +0100

    add cors

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const ExceptionResponse_1 = __importDefault(require("./Tools/ExceptionResponse"));
 const Router_1 = __importDefault(require("./Router"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 //set cookie parser :
 app.use((0, cookie_parser_1.default)());
+//set cors :
+app.use((0, cors_1.default)());
 //set routers :
 app.use('/api', Router_1.default);
 //set error response middelware :
